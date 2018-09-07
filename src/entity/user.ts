a70d985d0d5a9c0c2a9f88c9641b1f2e6c965dfe
Lanseria <zhangchao564265135@hotmail.com>
2018-09-07 22:25:25 +0800

    tabs.size = 2

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -3,19 +3,19 @@ import { Length, IsEmail } from 'class-validator';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({
-        length: 80
-    })
-    @Length(10, 80)
-    name: string;
+  @Column({
+    length: 80
+  })
+  @Length(10, 80)
+  name: string;
 
-    @Column({
-        length: 100
-    })
-    @Length(10, 100)
-    @IsEmail()
-    email: string;
+  @Column({
+    length: 100
+  })
+  @Length(10, 100)
+  @IsEmail()
+  email: string;
 }
